691bccfcb9c10db9ec28cb5d5de8b48a6d3e3813
chenzhiwei0109 <1138476755@qq.com>
2020-04-11 18:14:29 +0800

    admin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 
 module.exports = {
+    // base:'/learningblog/',
     title: '学习笔记', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
     description: '前端,HTML,CSS,webpack,前端学习,前端面试', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
